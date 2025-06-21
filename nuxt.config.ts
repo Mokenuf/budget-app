18ff63adb352f1eb65ab418f8c48a9ff98568c82
Francisco Racciatti <mokenuf@me.com>
2025-06-21 11:13:13 -0300

    add-i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@pinia/nuxt']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n'
+  ]
 })
