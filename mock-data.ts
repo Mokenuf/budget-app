71c10ecb7e62d95d528615e38013ce3205378abd
Francisco Racciatti <mokenuf@me.com>
2025-06-21 18:17:14 -0300

    feat(mock-data): add mock data

---
+++ mock-data.ts
@@ -0,0 +1,36 @@
+import Income from './shared/models/income'
+import Expense from './shared/models/expense'
+
+export const MOCK_INCOMES: Income[] = [
+  new Income({
+    id: 1,
+    name: 'Salario',
+    description: 'Salario mensual',
+    amount: 1875000,
+    type: 'fixed',
+  }),
+  new Income({
+    id: 2,
+    name: 'Freelance',
+    description: 'Proyecto web',
+    amount: 300000,
+    type: 'extra',
+  }),
+]
+
+export const MOCK_EXPENSES: Expense[] = [
+  new Expense({
+    id: 1,
+    name: 'Alquiler',
+    description: 'Depto 2 amb',
+    amount: 750000,
+    frequency: 'monthly',
+  }),
+  new Expense({
+    id: 2,
+    name: 'Wowsito',
+    description: 'Sub anual del wow',
+    amount: 100000,
+    frequency: 'yearly',
+  }),
+]
