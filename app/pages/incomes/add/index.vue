a319ec2b1e404e45dfd5009ad75ddc2a28ac3037
Francisco Racciatti <mokenuf@me.com>
2025-06-22 19:05:09 -0300

    feat(income-form): implement income form
    added income form and connections to edit and remove

---
+++ app/pages/incomes/add/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-bold mb-2">
+      {{ $t('pages.incomes.add.title') }}
+    </h2>
+    <FormIncome @submit="onSubmit" />
+  </div>
+</template>
+
+<script setup lang="ts">
+function onSubmit(data) {
+  console.log(data)
+}
+</script>
